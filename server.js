6223290e67a9358e3b3b3571ae7e7f60d4975a10
Rohit Joshi <rohitjoshi05.np@gmail.com>
2025-06-25 14:53:47 +0545

    Added MongoDB database

--- server.js
+++ server.js
@@ -1,64 +1,71 @@
+const mongoose = require('mongoose');
+const Task = require('./models/Task');
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error", err));
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let tasks = [];
-let idCounter = 1;
-
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
-});
-
-app.post('/tasks', (req, res)=> {
-    const{ title, description, priority, deadline, status } = req.body;
-    const newTask = { id: idCounter++, title, description, priority, deadline, status };
-    tasks.push(newTask);
-    res.status(201).json(newTask);
-});
-
-app.get('/tasks/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === id);
-
-    if(!task){
-        return res.status(404).json({error: "Task not found"});
-
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({error: "Failed to fetch tasks"});
     }
-    res.json(task);
 });
 
-app.put('/tasks/:id', (req, res) =>{
-    const id = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === id);
+app.post('/tasks', async(req, res)=> {
+    try {
+        const{ title, description, priority, deadline, status } = req.body;
+        const newTask = new Task({title, description, priority, deadline, status });
+        await newTask.save();
+        res.status(201).json(newTask);
 
-    if(!task){
-        return res.status(404).json({error: "Task not found"});
+    } catch (error) {
+    res.status(400).json({error: "Failed to create tasks."});
     }
-
-    const { title, description, priority, deadline, status } = req.body;
-    if (title !== undefined) task.title = title;
-    if (description !== undefined) task.description = description;
-    if (priority !== undefined) task.priority = priority;
-    if (deadline !== undefined) task.deadline = deadline;
-    if (status !== undefined) task.status = status;
-
-    res.json({message: "Task updated successfully", task});
 });
 
-app.delete('/tasks/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = tasks.findIndex(t => t.id === id);
-
-    if(index === -1){
-        return res.status(404).json({error: "Task not found"});
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if(!task){
+            return res.status(404).json({error: "Task not found"});
+        }
+        res.json(task);    
+    } catch (error) {
+        res.status(500).json({error:"Task not found or Invalid ID"});
     }
+});
 
-    tasks.splice(index, 1);
-    res.json({message: "Task deleted successfully"});
+app.put('/tasks/:id', async (req, res) =>{
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators:true});
+        if (!updatedTask){
+            return res.status(404).json({error : "Task not found"})
+        }
+        res.json({message: "Task updated successfully", task: updatedTask});
+    } catch (error) {
+        res.status(400).json({error: "Failed to update task."})
+    }
+});
 
-
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if(!deletedTask){
+            return res.status(404).json({error: "Task not found"});
+        }
+        res.json({message: "Task deleted successfully"});
+    } catch (error) {
+        res.status(500).json({error: "Failed to delete task."})
+    }
 });
 
 
